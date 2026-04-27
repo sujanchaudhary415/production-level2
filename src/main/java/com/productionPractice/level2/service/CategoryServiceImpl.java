c7c160d95bfa271b2c4fd334d37746f435c10252
sujanchaudhary415 <108916550+sujanchaudhary415@users.noreply.github.com>
2026-04-27 10:40:41 +0545

    added updateCategory feature

--- src/main/java/com/productionPractice/level2/service/CategoryServiceImpl.java
+++ src/main/java/com/productionPractice/level2/service/CategoryServiceImpl.java
@@ -17,7 +17,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
